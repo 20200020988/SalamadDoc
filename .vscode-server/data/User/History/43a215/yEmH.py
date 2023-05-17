afa48aa0f0e4e308248cbdca7b369cd88d3ec737
vagrant <vagrant@ubuntu2004.localdomain>
2023-05-17 14:29:32 +0000

    added function to browse through other pages

---
+++ .vscode-server/data/User/History/43a215/yEmH.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+def home (request):
+    return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == "POST":
+        patient_name = request.POST['patient_name']
+        patient_email = request.POST['patient_email']
+        patient_subject = request.POST['patient_subject']
+        patient_message = request.POST['patient_message']
+
+        send_mail(
+            patient_subject, 
+            patient_message,  
+            patient_email,  
+            ['kylenabo45@gmail.com'],  
+        )
+        return render(request, 'contact.html', {})
+    else:
+        return render(request, 'contact.html', {})
